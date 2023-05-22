f4254167cd6c9cc23a931bd4b7d8606f143ce7c4
Caio Domingos <caiod.dev@gmail.com>
2023-05-22 15:53:54 -0300

    home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  menuItems = [
+    {
+      name: 'Exercícios',
+      icon: 'list',
+      route: '/exercises',
+    },
+    {
+      name: 'Métodos',
+      icon: 'list',
+      route: '/methods',
+    },
+    {
+      name: 'Treinos',
+      icon: 'list',
+      route: '/exercise-set',
+    },
+    {
+      name: 'Agrup. de Treinos',
+      icon: 'list',
+      route: '/workouts',
+    },
+  ];
+  constructor(private router: Router) {}
 
+  goTo(route: string) {
+    this.router.navigateByUrl(route);
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,17 +1,12 @@
+import { MaterialBundleModule } from './../../core/material-bundle/material-bundle.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, MaterialBundleModule],
 })
-export class HomeModule { }
+export class HomeModule {}
