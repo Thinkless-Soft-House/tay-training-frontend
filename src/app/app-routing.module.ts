25753beefd4c9726e9991e18939de7b799effa38
Caio Domingos <caiod.dev@gmail.com>
2023-10-23 14:06:38 -0300

    new planner configs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,12 +17,49 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
   },
-  { path: 'master-details', loadChildren: () => import('./pages/master-details/master-details.module').then(m => m.MasterDetailsModule) },
-  { path: 'exercises', loadChildren: () => import('./pages/exercises/exercises.module').then(m => m.ExercisesModule) },
-  { path: 'workouts', loadChildren: () => import('./pages/workouts/workouts.module').then(m => m.WorkoutsModule) },
-  { path: 'methods', loadChildren: () => import('./pages/methods/methods.module').then(m => m.MethodsModule) },
-  { path: 'exercise-set', loadChildren: () => import('./pages/exercise-set/exercise-set.module').then(m => m.ExerciseSetModule) },
-  { path: 'planner', loadChildren: () => import('./pages/planner/planner.module').then(m => m.PlannerModule) },
+  {
+    path: 'master-details',
+    loadChildren: () =>
+      import('./pages/master-details/master-details.module').then(
+        (m) => m.MasterDetailsModule
+      ),
+  },
+  {
+    path: 'exercises',
+    loadChildren: () =>
+      import('./pages/exercises/exercises.module').then(
+        (m) => m.ExercisesModule
+      ),
+  },
+  {
+    path: 'workouts',
+    loadChildren: () =>
+      import('./pages/workouts/workouts.module').then((m) => m.WorkoutsModule),
+  },
+  {
+    path: 'methods',
+    loadChildren: () =>
+      import('./pages/methods/methods.module').then((m) => m.MethodsModule),
+  },
+  {
+    path: 'exercise-set',
+    loadChildren: () =>
+      import('./pages/exercise-set/exercise-set.module').then(
+        (m) => m.ExerciseSetModule
+      ),
+  },
+  {
+    path: 'planner',
+    loadChildren: () =>
+      import('./pages/planner/planner.module').then((m) => m.PlannerModule),
+  },
+  {
+    path: 'planner-v2',
+    loadChildren: () =>
+      import('./pages/planner-v2/planner-v2.module').then(
+        (m) => m.PlannerV2Module
+      ),
+  },
 ];
 
 @NgModule({
