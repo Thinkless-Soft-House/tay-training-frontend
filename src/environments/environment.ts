bf5348261c87634290ca2be50f237a5a3a7a74e7
Caio Domingos <caiod.dev@gmail.com>
2023-12-04 08:15:20 -0300

    Atualizando dist

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 
   // apiUrl: 'https://d37i1mypp2.execute-api.us-east-1.amazonaws.com/dev/api',
-  apiUrl: 'https://treino.taytraining.com.br/api',
+  apiUrl: 'https://tay-frontend.thinkless.com.br/api',
 };
