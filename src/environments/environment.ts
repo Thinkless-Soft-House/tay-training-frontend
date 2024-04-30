98fafced716ccf55e8bfc88c062757c97e4f753d
Caio Domingos <caio.domingos@thinkless.com.br>
2024-04-30 00:20:09 -0300

    env apiUrl change
    Signed-off-by: Caio Domingos <caio.domingos@thinkless.com.br>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
 
   // apiUrl: 'https://d37i1mypp2.execute-api.us-east-1.amazonaws.com/dev/api',
-  apiUrl: 'https://tay-frontend.thinkless.com.br/api',
+  // apiUrl: 'https://tay-frontend.thinkless.com.br/api',
+  apiUrl: 'https://treinos-server.taytraining.com.br/api',
 };
